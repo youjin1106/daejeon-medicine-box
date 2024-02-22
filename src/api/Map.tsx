3e7cbeae4fd1568f59c9182b1da652c614a84e78
jyj <jjnote22@gmail.com>
2024-02-22 12:17:46 +0900

    추가:아이콘 마커,상세정보 마커 추가

--- src/api/Map.tsx
+++ src/api/Map.tsx
@@ -1,9 +1,16 @@
 import { useEffect, useRef } from "react";
+import InfoMarker from "../components/InfoMarker";
+import IconMarker from "../components/IconMarker";
 
 const Map = () => {
   let map: naver.maps.Map;
   const mapEl = useRef(null);
   const { naver } = window;
+
+  //임시 데이터
+  const title = "장소명";
+  const address = "내주소";
+  const telNumber = "234";
 
   useEffect(() => {
     if (!mapEl.current || !naver) return;
@@ -21,16 +28,23 @@ const Map = () => {
       },
     };
     map = new naver.maps.Map(mapEl.current, mapOptions);
-    // new naver.maps.Marker({
-    //   position: new naver.maps.LatLng(36.321655, 127.378953),
-    //   map,
-    // });
+    new naver.maps.Marker({
+      position: new naver.maps.LatLng(36.321655, 127.378953),
+      map,
+      icon: {
+        //
+        content: IconMarker(),
+        size: new naver.maps.Size(38, 58),
+        anchor: new naver.maps.Point(19, 58),
+      },
+      clickable: true,
+    });
   }, []);
   // map.setCenter(jeju)
 
   return (
     <>
-      <h1 className="m-20">대전 폐의약품 수거함 위치</h1>
+      <h1 className="m-10">대전 폐의약품 수거함 위치</h1>
       <div ref={mapEl} />
     </>
   );
