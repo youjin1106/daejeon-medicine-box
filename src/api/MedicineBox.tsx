53c77aca55174fb83df92d6371ae05db6a0607d3
jyj <jjnote22@gmail.com>
2024-02-25 17:24:05 +0900

    추가:모든 구의 GET URL 상수에 담음

--- src/api/MedicineBox.tsx
+++ src/api/MedicineBox.tsx
@@ -1,6 +1,14 @@
 import axios from "axios";
 const baseURL = "https://api.odcloud.kr/api";
 const AddressAPIKEY = import.meta.env.VITE_ADDRESS_KEY;
+const YUSEONGGU_GET_URL =
+  "/15078180/v1/uddi:d4d649cd-b320-4b97-b237-bd95b330eea9";
+//서구 위,경도 제공
+const SEOGU_GET_URL = "/15077806/v1/uddi:1207b449-cc87-4c0d-93d7-d9dfae695a22";
+const JUNGGU_GET_URL = "/15101285/v1/uddi:c12a8c10-3d51-4a6c-80ae-dc85acfbc100";
+const DAEDUCKGU_GET_URL =
+  "/15077823/v1/uddi:4e1b21e4-0b7a-431d-a576-e1c70a4dff3e";
+const DONGGU_GET_URL = "/15077803/v1/uddi:d6790275-7eda-46af-bfc0-e954c4d78e46";
 
 type LocationRequestResult = {
   도로명주소: string;
@@ -19,7 +27,7 @@ const getUAdress = async (
   array: Array<LocationRequestResult>
 ) => {
   const res = await axios({
-    url: `${baseURL}/15078180/v1/uddi:d4d649cd-b320-4b97-b237-bd95b330eea9`,
+    url: baseURL + YUSEONGGU_GET_URL,
     method: "GET",
     headers: {
       "Content-Type": "application/json",
---
+++ src/components/LocationMenu.tsx
@@ -0,0 +1,13 @@
+const LocationMenu = () => {
+  return (
+    <div className="flex flex-row ">
+      <div>서구</div>
+      <div>유성구</div>
+      <div>중구</div>
+      <div>대덕구</div>
+      <div>동구</div>
+    </div>
+  );
+};
+
+export default LocationMenu;
