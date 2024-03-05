f03bcc62d375dd9fb09abb93c1832d154a14b440
jyj <jjnote22@gmail.com>
2024-03-05 22:27:27 +0900

    추가:useGeoLocation 생성
    Map 컴포넌트와 연결하여 현재 위치의 위도, 경도 불러옴

--- src/components/Map.tsx
+++ src/components/Map.tsx
@@ -1,10 +1,12 @@
 import { useEffect, useRef, useState } from "react";
 import LocationMenu from "./LocationMenu";
+import useGeoLocation from "./useGeoLocation";
 
 const Map = () => {
   const mapEl = useRef(null);
   const { naver } = window;
   const [map, setMap] = useState<naver.maps.Map | null>();
+  const position = useGeoLocation();
   const initializeMap = () => {
     if (!mapEl.current || !naver) return;
     const firstLocation = new naver.maps.LatLng(36.355504, 127.383844);
@@ -30,6 +32,11 @@ const Map = () => {
 
   return (
     <>
+      <div>
+        {position.loaded
+          ? `${position.coordinates?.latitude}과 ${position.coordinates?.longitude}`
+          : "위치를 불러오는 중입니다..."}
+      </div>
       <div className="flex flex-row">{map && <LocationMenu map={map} />}</div>
       <div ref={mapEl} className="mx-auto" />
     </>
---
+++ src/components/useGeoLocation.ts
@@ -0,0 +1,46 @@
+import { useEffect, useState } from "react";
+
+type LocationType = {
+  loaded: boolean;
+  coordinates?: { latitude: number; longitude: number };
+  error?: string;
+};
+
+const useGeoLocation = () => {
+  const [position, setPosition] = useState<LocationType>({
+    loaded: false,
+    coordinates: { latitude: 36.355504, longitude: 127.383844 },
+  });
+
+  const getPositionSuccess = (position: {
+    coords: { latitude: number; longitude: number };
+  }) => {
+    setPosition({
+      loaded: true,
+      coordinates: {
+        latitude: position.coords.latitude,
+        longitude: position.coords.longitude,
+      },
+    });
+  };
+
+  const getPositionError = () => {
+    setPosition({
+      loaded: true,
+      error:
+        "현재 위치를 확인할 수 없습니다. 위의 메뉴에서 지역을 직접 선택해주세요.",
+    });
+  };
+  useEffect(() => {
+    if (!("geolocation" in navigator)) {
+      getPositionError();
+    }
+    navigator.geolocation.getCurrentPosition(
+      getPositionSuccess,
+      getPositionError
+    );
+  }, []);
+  return position;
+};
+
+export default useGeoLocation;
