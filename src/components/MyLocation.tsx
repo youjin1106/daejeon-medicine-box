0e66a2a83bd1dcd723b2d3105051eae4050e2457
jyj <jjnote22@gmail.com>
2024-03-08 23:52:26 +0900

    추가:내 위치 중심으로 지도 중심 이동하고 가까운 수거함 5개 이상 보여줌

--- src/components/MyLocation.tsx
+++ src/components/MyLocation.tsx
@@ -2,9 +2,11 @@ import {
   대덕구_수거함_좌표,
   동구_수거함_좌표,
   서구_수거함_좌표,
+  수거함_정보,
   유성구_수거함_좌표,
   중구_수거함_좌표,
 } from "../medicinboxCode";
+import IconMarker from "./IconMarker";
 import { MapProps } from "./LocationMenu";
 import useGeoLocation from "./useGeoLocation";
 
@@ -21,13 +23,16 @@ const MyLocation = (props: MapProps) => {
 
   const myLocation_x = position.coordinates!.latitude;
   const myLocation_y = position.coordinates!.longitude;
-  const markerList = [];
+  map.setCenter(new naver.maps.LatLng(myLocation_x, myLocation_y));
+  const markerList: 수거함_정보[] = [];
   const distanceList = [];
+  const infoWindows: naver.maps.InfoWindow[] = [];
+
   for (let i = 0; i < all_location.length; i += 1) {
     const distance =
       Math.abs(all_location[i].x - myLocation_x) +
       Math.abs(all_location[i].y - myLocation_y);
-    if (markerList.length < 10) {
+    if (markerList.length < 5) {
       markerList.push(all_location[i]);
       distanceList.push(distance);
     } else {
@@ -41,8 +46,58 @@ const MyLocation = (props: MapProps) => {
     }
   }
 
-  console.log(markerList);
-  console.log(distanceList);
+  for (let i = 0; i < markerList.length; i += 1) {
+    const marker = new naver.maps.Marker({
+      position: new naver.maps.LatLng(markerList[i].x, markerList[i].y),
+      map,
+      icon: {
+        content: IconMarker(),
+        size: new naver.maps.Size(38, 58),
+        anchor: new naver.maps.Point(15, 20),
+      },
+      clickable: true,
+      animation: naver.maps.Animation.DROP,
+    });
+
+    const infoWindow = new naver.maps.InfoWindow({
+      content: `<div class="flex flex-col bg-white w-fit p-[18px] border-solid border-[1px]">
+      <p class="font-bold text-xl mb-2">${markerList[i].위치명}</p>
+      <p>${markerList[i].도로명주소}</p>
+      <p>${validateTelNum(markerList[i].전화번호) && markerList[i].전화번호}</p>
+      </div>`,
+      borderWidth: 0,
+      disableAnchor: true,
+      backgroundColor: "transparent",
+    });
+
+    infoWindows.push(infoWindow);
+    naver.maps.Event.addListener(marker, "click", getClickHandler(i));
+  }
+  function getClickHandler(index: number) {
+    return function () {
+      const marker = markerList[index],
+        infoWindow = infoWindows[index];
+
+      if (infoWindow.getMap()) {
+        infoWindow.close();
+      } else {
+        infoWindow.open(map, new naver.maps.LatLng(marker.x, marker.y));
+      }
+    };
+  }
+
+  function validateTelNum(checkNumber: string | null | undefined) {
+    if (
+      checkNumber === "" ||
+      checkNumber === "undefined" ||
+      checkNumber === "null" ||
+      checkNumber === null ||
+      checkNumber === undefined
+    ) {
+      return "";
+    }
+    return checkNumber;
+  }
   return <div>test MyLocation</div>;
 };
 
