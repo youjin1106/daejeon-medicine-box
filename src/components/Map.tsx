cd8e849ba4d0413347ddc13261ce3976b078ca0a
jyj <jjnote22@gmail.com>
2024-03-11 17:44:57 +0900

    수정:switch문을 object literal로 리팩토링
    타입스크립트 적용 형식을 좀 더 고려해볼 필요가 있음

--- src/components/Map.tsx
+++ src/components/Map.tsx
@@ -1,19 +1,17 @@
 import { useEffect, useRef, useState } from "react";
 import LocationMenu from "./LocationMenu";
-import useGeoLocation from "./useGeoLocation";
 import MyLocation from "./MyLocation";
 
 const Map = () => {
   const mapEl = useRef(null);
   const { naver } = window;
   const [map, setMap] = useState<naver.maps.Map | null>();
-  // const position = useGeoLocation();
   const initializeMap = () => {
     if (!mapEl.current || !naver) return;
     const firstLocation = new naver.maps.LatLng(36.355504, 127.383844);
     const mapOptions = {
       center: firstLocation,
-      zoom: 16,
+      zoom: 14,
       minZoom: 14,
       maxZoom: 18,
       zoomControl: true,
@@ -33,12 +31,7 @@ const Map = () => {
 
   return (
     <>
-      <div>
-        {map && <MyLocation map={map} />}
-        {/* {position.loaded
-          ? `${position.coordinates?.latitude}과 ${position.coordinates?.longitude}`
-          : "위치를 불러오는 중입니다..."} */}
-      </div>
+      <div>{map && <MyLocation map={map} />}</div>
       <div className="flex flex-row">{map && <LocationMenu map={map} />}</div>
       <div ref={mapEl} className="mx-auto" />
     </>
